2ab77d9521df3fac0befabcfac14021e470cae42
Administrator <admin@example.com>
2018-11-19 18:42:33 +0800

    增加部署文件

---
+++ script/H5-pro-deploy_feiniao_tomcat.py
@@ -0,0 +1,30 @@
+# -*- coding:utf8 -*-
+import subprocess
+import json
+import sys
+import os
+
+# python3 -u H5-pro-deploy_feiniao_tomcat.py airbird-aliyun-prod/
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Usage: python3 H5-pro-deploy_feiniao_tomcat.py [project name]")
+        sys.exit()
+    
+    # copy setting files to current working directory
+    subprocess.call(["cp", "-R", sys.argv[1] + "_settings/.", "."])
+
+    with open("settings.json") as config_file:
+        config = json.load(config_file)
+    
+    project = config["project"]
+
+    # clone project from git
+    if (len(sys.argv) == 3):
+        subprocess.call(["git", "clone", "-b", sys.argv[2], "git@gitlab-root:root/%s.git" % project])
+        print ("new config")
+    else:
+        subprocess.call(["git", "clone", "git@gitlab-root:root/%s.git" % project])
+        print ("old config")
+
+    # 调用 shell 命令行 deployer 程序
+    subprocess.call(["deployer", "-config", "settings.json"])
